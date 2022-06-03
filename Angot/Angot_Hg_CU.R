d706f2db6e4a53ef98de602c1ed71bff7abad6f4
Daphne Virlar-Knight <virlar-knight@nceas.ucsb.edu>
2022-06-03 13:25:06 -0700

    Publish Hg dataset for Angot

--- Angot/Angot_Hg_CU.R
+++ Angot/Angot_Hg_CU.R
@@ -1,10 +1,11 @@
 # Daphne Virlar-Knight
-# May 26, 2022
+# May 31, 2022
 
 
 ## -- Tasks -- ## 
-# Change xml name and data file name per Byron's request
-# Don't forget to issue a new DOI once processing has been completed
+# [X] Change xml name per Byron's request
+# [X] Change file per Byron's request --> did this in web editor
+# [ ] Don't forget to issue a new DOI once processing has been completed
 
 
 ## -- Set Up -- ##
@@ -17,12 +18,66 @@ library(dataone)
 # Set nodes
 d1c <- dataone::D1Client("PROD", "urn:node:ARCTIC")
 
-# Get the package
-packageId <- "resource_map_doi:10.18739/A2BV79X0C"
+# Load Dataset
+packageId <- "resource_map_urn:uuid:b9e85638-2a23-41cb-a216-b4595e389273"
 dp  <- getDataPackage(d1c, identifier = packageId, lazyLoad=TRUE, quiet=FALSE)
 
-# Get the metadata id
 xml <- selectMember(dp, name = "sysmeta@fileName", value = ".xml")
 
-# Read in the metadata
 doc <- read_eml(getObject(d1c@mn, xml))
+
+
+
+# change object name (physical) to match entity name
+doc$dataset$dataTable$physical$objectName <- doc$dataset$dataTable$entityName
+
+eml_validate(doc)
+# awesome, that didn't cause any glitches
+
+
+## -- publish package w/new xml name -- ##
+# Write EML
+eml_path <- "~/Scratch/Hg0_CU_ng_m3_30min_avg.xml"
+write_eml(doc, eml_path)
+
+
+# change access rules
+myAccessRules <- data.frame(subject="CN=arctic-data-admins,DC=dataone,DC=org",
+                            permission="changePermission")
+
+# generate doi
+doi <- dataone::generateIdentifier(d1c@mn, "DOI")
+
+# replace eml and add doi
+dp <- replaceMember(dp, xml, replacement=eml_path, newId=doi)
+
+# make public
+newPackageId <- uploadDataPackage(d1c, dp, 
+                                  accessRules = myAccessRules,
+                                  public=TRUE, quiet=FALSE)
+
+
+## -- set rights and access -- ## 
+# Manually set ORCiD
+
+# Helene
+subject1 <- 'http://orcid.org/0000-0003-4673-8249'
+
+# Byron
+subject2 <- 'http://orcid.org/0000-0002-3366-6269'
+
+# Get data pids
+ids <- getIdentifiers(dp)
+
+# set rights for Byron
+set_rights_and_access(d1c@mn,
+                      pids = c(ids, newPackageId),
+                      subject = subject1,
+                      permissions = c('read', 'write', 'changePermission'))
+
+
+# set access for Helene
+set_access(d1c@mn, c(ids, newPackageId), subject2)
+
+
+
