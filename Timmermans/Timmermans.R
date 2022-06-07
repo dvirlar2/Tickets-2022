1f4fec3c3643532f18bfe732cc608d52cd270405
Daphne Virlar-Knight <virlar-knight@nceas.ucsb.edu>
2022-06-07 12:31:50 -0700

    published Timmermans w/DOI

--- Timmermans/Timmermans.R
+++ Timmermans/Timmermans.R
@@ -3,7 +3,8 @@
 
 
 ## -- Tasks -- ## 
-# Copy over attributes from one file to another
+# [X] Copy over attributes from one file to another
+# [X] Publish dataset with a DOI
 
 
 ## -- Set Up -- ##
@@ -17,7 +18,7 @@ library(dataone)
 d1c <- dataone::D1Client("PROD", "urn:node:ARCTIC")
 
 # Load target dataset
-packageId <- "resource_map_urn:uuid:6c0a1b3d-dc73-4fc3-89ee-755f7253c3fa"
+packageId <- "resource_map_urn:uuid:789c8ca5-e552-40e8-af00-ede557f83d1d"
 dp  <- getDataPackage(d1c, identifier = packageId, lazyLoad=TRUE, quiet=FALSE)
 
 xml <- selectMember(dp, name = "sysmeta@fileName", value = ".xml")
@@ -56,3 +57,37 @@ attributeList$attributes$definition[[25]] <- "Number of scans per bin"
 attributeList$attributes$definition[[26]] <- "Flag code for each cast site"
 
 doc$dataset$dataTable[[1]]$attributeList <- attributeList
+
+
+
+# -- publish the package -- ##
+# write EML
+eml_path <- "~/Scratch/Conductivity-Temperature-Depth_(CTD)_and_Geochemistry_Data_from.xml"
+write_eml(doc, eml_path)
+
+# generate doi
+doi <- dataone::generateIdentifier(d1c@mn, "DOI")
+
+# replace eml and add doi
+dp <- replaceMember(dp, xml, replacement=eml_path, newId=doi)
+
+# Set access rules
+myAccessRules <- data.frame(subject="CN=arctic-data-admins,DC=dataone,DC=org",
+                            permission="changePermission")
+
+# Publish package with DOI
+newPackageId <- uploadDataPackage(d1c, dp, public=TRUE,
+                                  accessRules = myAccessRules, quiet=FALSE)
+
+## -- set rights & access -- ##
+# Setting access to all PIDs associated with package
+subject <- 'http://orcid.org/0000-0003-2718-2556'
+
+
+# get list of current pids
+pids <- arcticdatautils::get_package(d1c@mn, packageId)
+
+set_rights_and_access(d1c@mn,
+                      pids = c(xml, pids$data, packageId),
+                      subject = subject,
+                      permissions = c('read', 'write', 'changePermission'))
