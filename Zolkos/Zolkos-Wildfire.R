ebe5781598c2de00bc5f8356b06f5894cd2481f1
Daphne Virlar-Knight <virlar-knight@nceas.ucsb.edu>
2022-07-11 17:48:46 -0700

    Fixed custom units & published with new DOI method
    New DOI method included reinstalling arcticdatautils, and running lines 174-175

--- Zolkos/Zolkos-Wildfire.R
+++ Zolkos/Zolkos-Wildfire.R
@@ -17,7 +17,7 @@ library(EML)
 # get nodes
 d1c <- dataone::D1Client("PROD", "urn:node:ARCTIC")
 
-packageId <- "resource_map_urn:uuid:aeb90c36-95ad-4298-894a-51bbb78fd8dc"
+packageId <- "resource_map_urn:uuid:a4b38680-b7ce-466c-a626-2781f2c0ffbd"
 dp <- getDataPackage(d1c, identifier = packageId, lazyLoad=TRUE, quiet=FALSE)
 
 xml <- selectMember(dp, "sysmeta@fileName", ".xml")
@@ -70,17 +70,31 @@ doc$dataset$dataTable$attributeList$attribute[[14]]$measurementScale$interval$nu
 eml_validate(doc)
 
 
+## -- add custom units -- ##
+# save enumerated domain info. For some reason it's not being retained in Shiny.
+month_att <- doc$dataset$dataTable$attributeList$attribute[[5]]
+day_att <- doc$dataset$dataTable$attributeList$attribute[[6]]
+burn_att <- doc$dataset$dataTable$attributeList$attribute[[11]]
+
 
 # adding custom units for:
-  # cond -- microsiemensPerCentimeter (uS/cm)
+  #X cond -- microsiemensPerCentimeter (uS/cm)
   # SUVA -- literPerMilligramPerMeter (L/mg/m)
-  # CO2atm, CO2ppm, CH4atm, CH4ppm -- partsPerMillion (ppm)
+  #X CO2atm, CO2ppm, CH4atm, CH4ppm -- partsPerMillion (ppm)
   # pCO2, pCH4 -- microatmospheres
   # CO2flux -- micromolePerMeterSquaredPerSecond (µmol/m^2/sec)
   # CH4flux -- nanomolePerMeterSquaredPerSecond (nmol/m^2/sec)
 atts <- get_attributes(doc$dataset$dataTable$attributeList)
 atts_edited <- shiny_attributes(attributes = atts$attributes)
 doc$dataset$dataTable$attributeList <- set_attributes(atts_edited$attributes)
+
+
+# assign back enumerated domains
+month_att <- doc$dataset$dataTable$attributeList$attribute[[5]]
+day_att <- doc$dataset$dataTable$attributeList$attribute[[6]]
+burn_att <- doc$dataset$dataTable$attributeList$attribute[[11]]
+
+
 
 
 # manually fix SUVA custom unit
@@ -112,3 +126,70 @@ myAccessRules <- data.frame(subject="CN=arctic-data-admins,DC=dataone,DC=org",
 
 newPackageId <- uploadDataPackage(d1c, dp, public = FALSE,
                                   accessRules = myAccessRules, quiet = FALSE)
+
+
+
+# ---------------------------------------------------------------------------- #
+# July 11, 2022 Update
+
+# Fix the SUVA custom unit
+atts <- get_attributes(doc$dataset$dataTable$attributeList)
+
+atts_edited <- shiny_attributes(attributes = atts$attributes)
+  # need to add back enumerated domain for month, day, burn
+
+custom_units <- atts_edited$units
+
+unitlist <- set_unitList(custom_units, as_metadata = TRUE)
+doc$additionalMetadata <- unitlist
+
+# get the enumerated domain for month, day, burn
+enumDomains <- doc$dataset$dataTable$attributeList
+
+
+# set the edited atts back to the doc
+doc$dataset$dataTable$attributeList <- set_attributes(attributes = atts_edited$attributes,
+                                                      factors = atts_edited$factors)
+
+# check on missing value codes
+doc$dataset$dataTable$attributeList$attribute[[23]]$missingValueCode
+  # random numbers checked -- looks good!
+
+# check on enumerated domains
+# Month
+doc$dataset$dataTable$attributeList$attribute[[5]]$measurementScale <- enumDomains$attribute[[5]]$measurementScale
+
+# Day
+doc$dataset$dataTable$attributeList$attribute[[6]]$measurementScale <- enumDomains$attribute[[6]]$measurementScale
+
+# Burn
+doc$dataset$dataTable$attributeList$attribute[[11]]$measurementScale <- enumDomains$attribute[[11]]$measurementScale
+
+eml_validate(doc)
+  # TRUE 
+
+
+
+# -- add distribution -- #
+id <- generateIdentifier(d1c@mn, "doi")
+doc <- eml_add_distribution(doc, id)
+
+eml_validate(doc)
+
+
+# -- publish the package -- ##
+# write EML
+eml_path <- "~/Scratch/Wildfire_effects_on_aquatic_chemistry_Yukon.xml"
+write_eml(doc, eml_path)
+
+
+# replace eml and add doi
+dp <- replaceMember(dp, xml, replacement=eml_path, newId=id)
+
+# Set access rules
+myAccessRules <- data.frame(subject="CN=arctic-data-admins,DC=dataone,DC=org",
+                            permission="changePermission")
+
+# Publish package with DOI
+newPackageId <- uploadDataPackage(d1c, dp, public=TRUE,
+                                  accessRules = myAccessRules, quiet=FALSE)
