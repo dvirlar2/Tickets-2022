97707c70f02d469d806d491b4def9967ee18237a
Daphne Virlar-Knight <virlar-knight@nceas.ucsb.edu>
2022-08-10 15:43:15 -0700

    Continued to process Schwoerer ticket
    Edited shapefile format ID and physical format name, and added FAIR publishing info

--- Schwoerer/Ticket-Processing_Spatial-Vector.R
+++ Schwoerer/Ticket-Processing_Spatial-Vector.R
@@ -10,7 +10,7 @@ library(EML)
 # get nodes
 d1c <- dataone::D1Client("PROD", "urn:node:ARCTIC")
 
-packageId <- "resource_map_urn:uuid:d2c0985a-fc0b-4740-b8a2-d20cf6c75883"
+packageId <- "resource_map_urn:uuid:caddc5b2-1bb5-4e44-95d7-881f2b040044"
 dp <- getDataPackage(d1c, identifier = packageId, lazyLoad=TRUE, quiet=FALSE)
 
 
@@ -57,6 +57,31 @@ spatialVector <- pid_to_eml_entity(d1c@mn,
 doc$dataset$spatialVector[[1]] <- spatialVector
 doc$dataset$otherEntity[[2]] <- NULL
 eml_validate(doc)
+
+
+## -- edit format ID -- ##
+# Since the spatial vector formula creates a physical based off of this,
+# should probably make changing the format id a higher step on the to-do list.
+# That way you don't have to edit the format id AND physical :facepalm:
+
+# format id
+vector_pid <- selectMember(dp, "sysmeta@fileName", ".zip")
+sysmeta <- getSystemMetadata(d1c@mn, vector_pid)
+sysmeta@formatId <- "application/vnd.shp+zip"
+  
+updateSystemMetadata(d1c@mn, vector_pid, sysmeta)
+
+
+# physical format name
+doc$dataset$spatialVector$physical$dataFormat$externallyDefinedFormat$formatName <- "application/vnd.shp+zip"
+
+eml_validate(doc)
+
+
+## -- add FAIR -- ##
+doc <- eml_add_publisher(doc)
+doc <- eml_add_entity_system(doc)
+
 
 
 ## -- update package -- ##
